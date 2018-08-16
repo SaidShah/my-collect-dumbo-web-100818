a7a8d75061a18a5ba792be587a16735024459794
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-16 19:41:53 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(collection) 
+  if(block_given?)
+counter = 0 
+new_array = []
+while counter < collection.length
+ new_array << yield(collection[counter])
+  counter += 1 
+end
+end
+  new_array
+end
