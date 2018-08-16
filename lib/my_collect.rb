110e3195aa2eb6b3276303e9b1785d817a5aa6d5
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-16 19:40:49 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,9 @@
 def my_collect(collection) 
+  if(block_given?)
 counter = 0 
+new_array = []
 while counter < collection.length-1
-  yield(collection[counter])
+ new_array << yield(collection[counter])
   counter += 1 
 end
 
